7d8d0b11fdf5ff90e0ff7998efb2e1c57dd9f3d2
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-09-22 02:23:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,4 @@ pkg update
 pkg upgrade -y
 pkg install libjansson wget nano -y
 mkdir ccminer && cd ccminer
-wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
-wget https://raw.githubusercontent.com/Lexa00heda/verusMinig/refs/heads/main/config.json
-wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
 chmod +x ccminer start.sh
