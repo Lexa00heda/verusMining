83135a0f7a2028e8fdcf2a1a26f52f391ceae1ec
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-09-29 17:06:41 +0530

    Create rdpSetup.sh

---
+++ rdpSetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt update
+apt install python3-pip -y
+pip install selenium
+wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
+apt-get install tar -y
+sudo tar -xvf geckodriver-v0.35.0-linux64.tar.gz
+sudo mv geckodriver /usr/local/bin/
+chmod +x /usr/local/bin/geckodriver
+wget https://raw.githubusercontent.com/Lexa00heda/verusMining/refs/heads/main/sele.py
+pip3 install pyotp
+wget https://raw.githubusercontent.com/Lexa00heda/verusMining/refs/heads/main/sele.py
+sudo apt-get update
+sudo apt-get install android-sdk -y
+wget https://developer.samsung.com/remotetestlab/webclient/assets/linux/rdb.zip
+unzip rdb.zip
+./rdb.zip
