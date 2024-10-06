82e7be5441aef935ef70328f49a5999e92475bf5
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-06 13:37:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ pkg upgrade -y
 pkg install libjansson wget nano -y
 pkg install tmux -y
 chmod +x ccminer start.sh
-tmux new-session -d -s apiserver 'yes |nohup ./start.sh  /dev/null ./logFile 2>&1 &'
+tmux new-session -d -s apiserver 'yes |nohup ./start.sh  /dev/null ./logFile 2>&1'
