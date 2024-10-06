90c0c5894040364b21ccedb23951590dd542a244
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-06 13:07:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ termux-wake-lock
 pkg update
 pkg upgrade -y
 pkg install libjansson wget nano -y
+pkg install tmux -y
+tmux new-session -d -s apiserver 'yes |nohup ./install.sh  /dev/null'
 chmod +x ccminer start.sh
 ./start.sh
