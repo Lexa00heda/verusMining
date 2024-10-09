19d2ba3b5d02d9c2a46783f82781692a056c2a75
Lexa00heda <120236068+Lexa00heda@users.noreply.github.com>
2024-10-09 20:49:24 +0530

    Update rdpSetup.sh

--- rdpSetup.sh
+++ rdpSetup.sh
@@ -8,6 +8,7 @@ sudo tar -xvf geckodriver-v0.35.0-linux64.tar.gz
 sudo mv geckodriver /usr/local/bin/
 chmod +x /usr/local/bin/geckodriver
 wget https://github.com/termux/termux-app/releases/download/v0.119.0-beta.1/termux-app_v0.119.0-beta.1+apt-android-7-github-debug_arm64-v8a.apk
+wget https://dl.aecoe.xyz/PandaAdmin/android/panda_pro_normal_7.0.3_175_1391_09201530_release.apk
 mv termux-app_v0.119.0-beta.1+apt-android-7-github-debug_arm64-v8a.apk termux.apk
 wget https://raw.githubusercontent.com/Lexa00heda/verusMining/refs/heads/main/sele.py
 pip3 install pyotp
